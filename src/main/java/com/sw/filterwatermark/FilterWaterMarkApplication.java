ca11b4389ac0fc5493ec664cbc1dfb1838ae3c7e
wanghao <281463547@qq.com>
2022-05-27 17:33:49 +0800

    项目初始化

---
+++ src/main/java/com/sw/filterwatermark/FilterWaterMarkApplication.java
@@ -0,0 +1,13 @@
+package com.sw.filterwatermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterWaterMarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilterWaterMarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw/filterwatermark/FilterWaterMarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.filterwatermark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilterWaterMarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
